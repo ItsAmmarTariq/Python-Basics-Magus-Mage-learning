dfc042dcf5ebce25348543f46cc22cbdb9d196d5
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-08 11:34:32 +0500

    working on variables

---
+++ variables.py
@@ -0,0 +1,56 @@
+### okkk 
+
+a=6
+print(type(a))
+
+# class 'int'
+b=7.000
+print(type(b))
+
+print(type(a+b))
+
+# this is also a float because of bigger float clss
+
+print(type(True))
+
+print(type('hello'))
+
+# age=int(input('Enter your age'))
+# if age>=18:
+#     print(f"you can vote")
+# else:
+#     print(f"you cannot vote")
+
+_st='hello world'
+
+for i in _st:
+    print(i)
+
+
+#####  complex data types  #########
+
+
+# real=5
+# imgnry=7
+
+# z=complex(real,imgnry)
+
+# print(f"the real part is {z.real}  and the imagnery part is {z.imag}")
+
+
+
+z=6+3j
+
+print(f"the real part is {z.real}  and the imagnery part is {z.imag}")
+
+fl=5.8
+
+s=''
+lis=[1,2,3,4]
+
+for i in lis:
+    s+=str(i)
+
+inte=int(''.join(s)) 
+
+print(type(inte))
