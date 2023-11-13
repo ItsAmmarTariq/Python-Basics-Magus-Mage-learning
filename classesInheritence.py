c7523b1891e468e8a5f88b4e217cb77729dac220
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-13 12:23:27 +0500

    done with practice of inheritence

--- classesInheritence.py
+++ classesInheritence.py
@@ -1,6 +1,35 @@
-class Car:
-    speed=0
-    started=False
+# class Car:
+#     speed=0
+#     started=False
+#     def start(self):
+#         self.started=True
+#         print('The car has started')
+    
+#     def Speed(self,val):
+#         if self.started:
+#             self.speed=val
+#             print('vroooooooooooooooommm!  ')
+#             print('Speed is now ',val)
+#         else:
+#             print('You need to start the car \'First\' ')
+    
+#     def stop(self):
+#         self.speed=0
+#         print('Breaking......................!')
+#         print('************Stoped*****************')
+#         print('the speed is now ',self.speed)
+
+# obj=Car()
+# obj.start()
+# obj.Speed(40)
+# obj.stop()
+
+
+class Vehicle:
+    def __init__(self,speed=0,started=False):
+        self.started=started
+        self.speed=speed
+
     def start(self):
         self.started=True
         print('The car has started')
@@ -19,11 +48,27 @@ class Car:
         print('************Stoped*****************')
         print('the speed is now ',self.speed)
 
-obj=Car()
-obj.start()
-obj.Speed(40)
-obj.stop()
+
+class Car(Vehicle):
+    trunk_open=False
+    def open_trunk(self):
+        self.trunk_open=True
+    def close_trunk(self):
+        self.trunk_open=False
 
 
+class Motorcycle(Vehicle):
+    def __init__(self, centre_stand_out=False):
+        self.centre_stand_out=centre_stand_out
+        super().__init__()
+    
+
+        #override the super method 
+    def start(self):
+        # super().start()
+        print('Sorry ran out of fuel !')
 
 
+motorcycle=Motorcycle()
+motorcycle.start()
+print(motorcycle.started)
