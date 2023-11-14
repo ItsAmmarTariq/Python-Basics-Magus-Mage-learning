2fda8d8a8a215695d6882031222df421a4692939
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-14 14:56:16 +0500

    done abstraction in python

--- abstraction.py
+++ abstraction.py
@@ -2,13 +2,31 @@ from abc import ABC,abstractmethod
 
 class Father(ABC):
     @abstractmethod
-    def disp(self):
+    def disp1(self):
+        pass
+    
+    @abstractmethod
+    def disp2(self):
         pass
 
+    
+
+
 class Child(Father):
-    def disp(self):
+    def disp1(self):
         print('child class')
         print('defining the ABstarct method')
 
-c=Child()
-c.disp()
+        # we cant instantiate it becoz it is also a abstaract calss
+    
+    
+class GrandChild(Child):
+    def disp2(self):
+        print('this is disp2')
+    
+     
+    
+
+gc=GrandChild()
+gc.disp2()
+
