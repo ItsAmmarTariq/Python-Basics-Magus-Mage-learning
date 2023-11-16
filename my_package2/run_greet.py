8be8d87b09d02321923324492792ed248cf4fca1
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-16 12:43:11 +0500

    poetry package learning and create my own package

---
+++ my_package2/run_greet.py
@@ -0,0 +1,11 @@
+# run_greet.py
+
+from my_package2.my_module import greet
+
+def main():
+    name = "Ammar"  # Replace with the desired name
+    greeting = greet(name)
+    print(greeting)
+
+if __name__ == "__main__":
+    main()
