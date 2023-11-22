a5932f82ab36ee039674af70f44322738be63f4d
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-22 12:09:32 +0500

    practice of threading in python

--- multithreadng_practice.py
+++ multithreadng_practice.py
@@ -16,20 +16,36 @@ def cal_cube(numbers):
 
 arr=[2,3,5,8]
 
+def heavy(n, myid):
+    for x in range(1, n):
+        for y in range(1, n):
+            x**y
+    print(myid, "is done")
+def threaded(n):
+    threads = []
+    for i in range(n):
+        t = threading.Thread(target=heavy, args=(500,i,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
 
 
 
 
 if __name__=="__main__":
     t=time.time()
-    t1=threading.Thread(target=cal_square,args=(arr,))
-    t2=threading.Thread(target=cal_cube,args=(arr,))
+    t1=threading.Thread(target=heavy,args=(500,[x for x in range(81)]))
+    
+    # t1=threading.Thread(target=cal_square,args=(arr,))
+    # t2=threading.Thread(target=cal_cube,args=(arr,))
 
     t1.start()
-    t2.start()
+    # t2.start()
 
     t1.join()
-    t2.join()
+    # t2.join()
     print('done in ',time.time()-t)
     print(' i have completed my tasks')
         
