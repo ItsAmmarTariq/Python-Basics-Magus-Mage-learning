1c4f75215e3f8e685f81fda8eeeb3c456ff58673
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-22 15:38:05 +0500

    practice of multiprocessing in python done

--- multiprocessing_practice.py
+++ multiprocessing_practice.py
@@ -8,30 +8,52 @@ def cal_square(numbers,result,v,q):
    
     for ind,n in enumerate(numbers):
         q.put(n*n)  
-      
+
+def add_balance(balance,lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        balance.value=balance.value+1
+        lock.release()
+
+def withdraw_balance(balance,lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        balance.value=balance.value-1
+        lock.release()
+
+
+
         
     
-# def cal_cube(numbers):
-#     for n in numbers:
-#         time.sleep(5)
-#         print('cube '+str(n*n*n))
+def cal_cube(numbers):
+    for n in numbers:
+        time.sleep(.0001)
+        print('cube '+str(n*n*n))
         
 
 if __name__=='__main__':
     numbers=[2,3,7,8,6]
+
+    balancec_value=multiprocessing.Value('i',200)
     result=multiprocessing.Array('i',5)
     v=multiprocessing.Value('d',0.0)
+    lock=multiprocessing.Lock()
 
     q=multiprocessing.Queue()
-    p1=multiprocessing.Process(target=cal_square,args=(numbers,result,v,q))
+    p1=multiprocessing.Process(target=add_balance,args=(balancec_value,lock))
+    p2=multiprocessing.Process(target=withdraw_balance,args=(balancec_value,lock))
 
     p1.start()
-    
+    p2.start()
 
     p1.join()
-    
+    p2.join()
+
+    print(balancec_value.value) 
     # print(result[:])
     # print(v.value)
-    while q.empty() is False:
-        print(q.get())
-    print('Done !')
+    # while q.empty() is False:
+    #     print(q.get())
+    # print('Done !')
