3a4e93ff7db147e1d96c955d91d47ee4bbfd5a4e
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-22 15:09:24 +0500

    practice of multiprocessing in python

--- multiprocessing_practice.py
+++ multiprocessing_practice.py
@@ -1,15 +1,15 @@
 import time
 import multiprocessing
 
-square_res=[]
 
-def cal_square(numbers):
-    global square_res
-    for n in numbers:
+
+def cal_square(numbers,result,v,q):
+    v.value=5.67
+   
+    for ind,n in enumerate(numbers):
+        q.put(n*n)  
+      
         
-        print('square '+str(n*n))
-        print('with in a process : result' +str(square_res))
-        square_res.append(n*n)
     
 # def cal_cube(numbers):
 #     for n in numbers:
@@ -18,14 +18,20 @@ def cal_square(numbers):
         
 
 if __name__=='__main__':
-    arr=[2,3,7,8,6]
-    p1=multiprocessing.Process(target=cal_square,args=(arr,))
-    #p2=multiprocessing.Process(target=cal_cube,args=(arr,))
+    numbers=[2,3,7,8,6]
+    result=multiprocessing.Array('i',5)
+    v=multiprocessing.Value('d',0.0)
+
+    q=multiprocessing.Queue()
+    p1=multiprocessing.Process(target=cal_square,args=(numbers,result,v,q))
 
     p1.start()
     
 
     p1.join()
     
-    print('result' +str(square_res))
+    # print(result[:])
+    # print(v.value)
+    while q.empty() is False:
+        print(q.get())
     print('Done !')
