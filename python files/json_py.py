12c526a2f8edd9b9e9534105821b2c28bf26d3a4
ItsAmmarTariq <ammarch935@gmail.com>
2023-11-29 16:40:08 +0500

    learning of excell manipulation in python using pandas and openpyxl

--- python files/json_py.py
+++ python files/json_py.py
@@ -1,6 +1,27 @@
 import json
 
-jsonstring = '{"name": "erik", "age": 38, "married": true}'
-person = json.loads(jsonstring)
-print(person['name'], 'is', person['age'], 'years old')
-print(type(person))
+
+def main():
+    jsonstring = '{"name": "erik", "age": 38, "married": true}'
+    person = json.loads(jsonstring)
+    print(person['name'], 'is', person['age'], 'years old')
+    json_str = json.dumps(jsonstring, indent=2)
+    # print(type(person))
+
+    print(json_str)
+    print(type(json_str))
+
+
+def jsonfileFunx():
+    with open('test.json') as json_file:
+        data = json.load((json_file))
+        print(data)
+
+
+def write_in_json():
+    data = {'name': 'test', 'age': 34}
+    with open('test.json', 'a') as file:
+        json.dump(data,file)
+
+
+jsonfileFunx()
